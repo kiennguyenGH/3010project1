2c71a9b5f0f6506f3c1a257b84f90f8b64d11554
kiennguyenGH <ktnguyen4647@gmail.com>
2022-10-09 18:59:51 -0700

    Prepare for Gaussian Elimination

--- GaussianElimination.java
+++ GaussianElimination.java
@@ -1,11 +1,90 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class GaussianElimination
 {
 
-    public void computeGaussian()
+    public static double findLargest(double[] row)
     {
+        double largest = 0;
+        for (int i = 0; i < row.length-1; i++)
+        {
+            if (Math.abs(row[i]) > largest)
+            {
+                largest = Math.abs(row[i]);
+            }
+        }
+        return largest;
+    }
 
+    public static double[] getColumn(double[][] matrix, int column)
+    {
+        double[] theColumn = new double[matrix.length];
+        for (int i = 0; i < theColumn.length; i++)
+        {
+            theColumn[i] = matrix[i][column];
+        }
+        return theColumn;
+    }
+
+    public static double[][] swapRows(double[][] matrix, int row1, int row2)
+    {
+        double[][] theMatrix = matrix;
+        double[] temp = matrix[row1];
+        theMatrix[row1] = theMatrix[row2];
+        theMatrix[row2] = temp;
+
+        return theMatrix;
+    }
+
+    public static double[] divideArray(double[] row, double[] scale)
+    {
+        double[] array = new double[row.length];
+        for (int i = 0; i < array.length; i++)
+        {
+            array[i] = row[i]/scale[i];
+        }
+        return array;
+    }
+
+    public static void printMatrix(double[][] matrix)
+    {
+        for (int i = 0; i < matrix.length; i++)
+        {
+            for (int k = 0; k < matrix[i].length; k++)
+            {
+                System.out.print(matrix[i][k] + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void printScales(double[] vector)
+    {
+        for (int i = 0; i < vector.length; i++)
+        {
+            System.out.print(vector[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void computeGaussian(double[][] matrix)
+    {
+        int pivotRow = 0;
+        double divideValue = 0;
+        int[] indices = new int[matrix[0].length-1];
+        double[] multipliers = new double[matrix.length];
+        double[] scales;
+        double[] largestValues = new double[matrix.length];
+        double[][] theMatrix = matrix;
+        for (int i = 0; i < largestValues.length; i++)
+        {
+            largestValues[i] = findLargest(matrix[i]);
+        }
+        for (int i = 0; i < matrix[0].length-1; i++)
+        {
+
+        }
     }
 
     public static double[][] getMatrix()
@@ -65,18 +144,11 @@ public class GaussianElimination
        }
        
        
-       //Get user rows
+       //Get user matrix
        if (theInput == 1)
        {
             double[][] test = getMatrix();
-            for (int i = 0; i < test.length; i++)
-            {
-                for (int k = 0; k < test[i].length; k++)
-                {
-                    System.out.print(test[i][k] + " ");
-                }
-                System.out.println();
-            }
+            computeGaussian(test);
             
             
         //Read file
